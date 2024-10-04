130bb57bc4c1237e61e247d8a0676c172cd93808
mSaito0217 <n.mmia0301@gmail.com>
2024-10-04 14:33:54 +0900

    First commit

---
+++ src/main/java/com/yamamotokogyo/ywm/YwmApplication.java
@@ -0,0 +1,13 @@
+package com.yamamotokogyo.ywm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YwmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YwmApplication.class, args);
+	}
+
+}
